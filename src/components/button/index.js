4be642c7ea0284dcbf206b106008955684d18ca3
Haleema1307 <haleemamughal15@gmail.com>
2022-07-04 17:15:39 +0500

    css

---
+++ src/components/button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./button.css";
+
+const Button = () => {
+  return (
+    <>
+      <button className="button">Set Position</button>
+    </>
+  );
+};
+export default Button;
---
+++ src/components/header-footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./header.css";
+
+const HeaderFooter = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="header">
+          <p>Header</p>
+        </div>
+        <div className="middle">
+          <p>middle</p>
+        </div>
+        <div className="footer">
+          <p>Footer</p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default HeaderFooter;
